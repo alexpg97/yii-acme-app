5676c3c9c4a02dc37fbe0ba3fa2809e1ced24797
Alex Perez <alex27_perez@hotmail.com>
2020-08-30 21:48:22 -0600

    Fix #1: Creating models and db migrations;

---
+++ models/User.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "user".
+ *
+ * @property int $id
+ * @property string $uid
+ * @property string $username
+ * @property string $email
+ * @property string $password
+ * @property int $status
+ * @property bool $contact_email
+ * @property bool $contact_phone
+ * @property string $created
+ * @property string $updated
+ *
+ * @property Message[] $fromMessages
+ * @property Message[] $toMessages
+ * @property PhoneNumber[] $phoneNumbers
+ * @property Trip[] $trips
+ */
+class User extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'user';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['uid', 'username', 'email', 'password', 'updated'], 'required'],
+            [['status'], 'default', 'value' => null],
+            [['status'], 'integer'],
+            [['contact_email', 'contact_phone'], 'boolean'],
+            [['created', 'updated'], 'safe'],
+            [['uid', 'password'], 'string', 'max' => 60],
+            [['username'], 'string', 'max' => 45],
+            [['email'], 'string', 'max' => 255],
+            [['email'], 'unique'],
+            [['uid'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'uid' => Yii::t('app', 'Uid'),
+            'username' => Yii::t('app', 'Username'),
+            'email' => Yii::t('app', 'Email'),
+            'password' => Yii::t('app', 'Password'),
+            'status' => Yii::t('app', 'Status'),
+            'contact_email' => Yii::t('app', 'Contact Email'),
+            'contact_phone' => Yii::t('app', 'Contact Phone'),
+            'created' => Yii::t('app', 'Created'),
+            'updated' => Yii::t('app', 'Updated'),
+        ];
+    }
+
+    /**
+     * Gets query for [[Messages]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getFromMessages()
+    {
+        return $this->hasMany(Message::className(), ['from_user_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[Messages0]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getToMessages0()
+    {
+        return $this->hasMany(Message::className(), ['to_user_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[PhoneNumbers]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPhoneNumbers()
+    {
+        return $this->hasMany(PhoneNumber::className(), ['user_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[Trips]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTrips()
+    {
+        return $this->hasMany(Trip::className(), ['user_id' => 'id']);
+    }
+}
